91644e61a07e908bba39bb97f836d25d2fc0d500
hupe1980 <hello@webappsolutions.de>
2019-06-09 22:27:43 +0200

    Misc

--- amplify-material-ui/babel.config.js
+++ amplify-material-ui/babel.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    presets: [['react-app', { flow: false, typescript: true }]],
-    env: {
-        production: {
-            plugins: [
-                ['react-remove-properties', { properties: ['data-test'] }],
-            ],
-        },
-    },
+    presets: [
+        [
+            'react-app',
+            { flow: false, typescript: true, absoluteRuntime: false },
+        ],
+    ],
+    // env: {
+    //     production: {
+    //         plugins: [
+    //             ['react-remove-properties', { properties: ['data-test'] }],
+    //         ],
+    //     },
+    // },
 };
