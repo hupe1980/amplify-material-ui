737b7e2030bcc6110c3a4f65efb52221ca4bacc1
hupe1980 <hello@webappsolutions.de>
2019-06-09 18:53:29 +0200

    chore: Update snapshot

--- amplify-material-ui/babel.config.js
+++ amplify-material-ui/babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     presets: [['react-app', { flow: false, typescript: true }]],
-    ignore: ['**/__tests__', '**/__stories__'],
     env: {
         production: {
+            ignore: ['**/__tests__', '**/__stories__'],
             plugins: [
                 ['react-remove-properties', { properties: ['data-test'] }],
             ],
