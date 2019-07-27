0039fdf02e7f55179a1cda406f6990dd6ba150a0
hupe1980 <hello@webappsolutions.de>
2019-07-27 07:40:38 +0200

    Fix export

--- amplify-material-ui/src/auth/index.tsx
+++ amplify-material-ui/src/auth/index.tsx
@@ -1,5 +1,5 @@
 export * from './authenticator';
 export * from './sign-in';
-export { default as Loading } from './loading';
+export * from './loading';
 export * from './with-authenticator';
 export * from './recaptcha';
