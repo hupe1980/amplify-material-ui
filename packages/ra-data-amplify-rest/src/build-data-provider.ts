44d17e6bc3b9f3dc32617b4bf9ad3493bdce2b92
hupe1980 <hello@webappsolutions.de>
2019-09-08 15:03:53 +0200

    fix: ApiMapping

--- packages/ra-data-amplify-rest/src/build-data-provider.ts
+++ packages/ra-data-amplify-rest/src/build-data-provider.ts
@@ -4,12 +4,6 @@ import { buildDataRequest } from './build-data-request';
 import { parseResponse } from './parse-response';
 import { RequestType, RequestParams, ApiCall, HttpMethod } from './types';
 
-const apiCallMap = new Map<HttpMethod, ApiCall>([
-    [HttpMethod.GET, API.get.bind(this)],
-    [HttpMethod.PUT, API.put.bind(this)],
-    [HttpMethod.POST, API.post.bind(this)],
-    [HttpMethod.DELETE, API.del.bind(this)],
-]);
 export interface BuildDataProviderOptions {
     apiName: string;
     buildDataRequest: typeof buildDataRequest;
@@ -26,11 +20,28 @@ export const buildDataProvider = (options: BuildDataProviderOptions) => {
     ) => {
         const { path, method, init } = buildDataRequest(type, resource, params);
 
-        const apiCall = apiCallMap.get(method);
+        let response;
 
-        if (!apiCall) throw new Error(`Unsupported http method ${method}`);
-
-        const response = await apiCall(apiName, path, init);
+        switch (method) {
+            case HttpMethod.GET: {
+                response = await API.get(apiName, path, init);
+                break;
+            }
+            case HttpMethod.PUT: {
+                response = await API.put(apiName, path, init);
+                break;
+            }
+            case HttpMethod.POST: {
+                response = await API.post(apiName, path, init);
+                break;
+            }
+            case HttpMethod.DELETE: {
+                response = await API.del(apiName, path, init);
+                break;
+            }
+            default:
+                throw new Error(`Unsupported http method ${method}`);
+        }
 
         return parseResponse(response, type, resource, params);
     };
