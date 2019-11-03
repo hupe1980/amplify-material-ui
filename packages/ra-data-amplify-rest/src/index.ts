b79e33b9d054320e197136c2cfe08fddc305e1e3
hupe1980 <frankhuebner1980@gmail.com>
2019-11-03 17:29:15 +0100

    Misc

--- packages/ra-data-amplify-rest/src/index.ts
+++ packages/ra-data-amplify-rest/src/index.ts
@@ -1,85 +1,88 @@
+import API from '@aws-amplify/api';
 import {
-    UPDATE_MANY,
-    DELETE_MANY,
-    UPDATE,
-    DELETE,
-    LegacyDataProvider,
-    convertLegacyDataProvider,
+    DataProvider,
+    GetListResult,
+    GetOneResult,
+    GetManyResult,
+    GetManyReferenceResult,
+    UpdateResult,
+    UpdateManyResult,
+    CreateResult,
+    DeleteResult,
+    DeleteManyResult,
 } from 'ra-core';
 
-import { buildDataRequest } from './build-data-request';
-import { parseResponse } from './parse-response';
-import {
-    buildDataProvider,
-    BuildDataProviderOptions,
-} from './build-data-provider';
+import { ResponseParser, defaultResponseParser } from './response-parser';
+import { RequestBuilder, defaultRequestBuilder } from './request-builder';
 
-import { Params } from './types';
-
-const defaultOptions = {
-    buildDataRequest,
-    parseResponse,
-};
-
-export interface DataProviderOptions extends Partial<BuildDataProviderOptions> {
+export interface AmplifyRestDataProviderOptions {
     apiName: string;
+    responseParser?: ResponseParser;
+    requestBuilder?: RequestBuilder;
 }
 
-export default (options: DataProviderOptions): LegacyDataProvider => {
-    const dataProvider = buildDataProvider({ ...defaultOptions, ...options });
+export const amplifyRestDataprovider = (
+    options: AmplifyRestDataProviderOptions,
+): DataProvider => {
+    const {
+        apiName,
+        responseParser = defaultResponseParser,
+        requestBuilder = defaultRequestBuilder,
+    } = options;
 
-    return convertLegacyDataProvider(
-        async (
-            type: string,
-            resource: string,
-            params: Params,
-        ): Promise<any> => {
-            if (type === UPDATE_MANY) {
-                const { ids, data, ...otherParams } = params;
-
-                if (!ids) return;
-
-                const responses = await Promise.all(
-                    ids.map(id =>
-                        dataProvider(UPDATE, resource, {
-                            data: {
-                                id,
-                                ...data,
-                            },
-                            ...otherParams,
-                        }),
-                    ),
-                );
-
-                return {
-                    data: responses.map(response => response.data),
-                };
-            }
-
-            if (type === DELETE_MANY) {
-                const { ids, ...otherParams } = params;
-
-                if (!ids) return;
-
-                const responses = await Promise.all(
-                    ids.map(id =>
-                        dataProvider(DELETE, resource, {
-                            id,
-                            ...otherParams,
-                        }),
-                    ),
-                );
-
-                return {
-                    data: responses.map(response => response.data),
-                };
-            }
-
-            return dataProvider(type, resource, params);
+    return {
+        async getList(resource, params): Promise<GetListResult> {
+            const { path, init } = requestBuilder.getList(resource, params);
+            const response = await API.get(apiName, path, init);
+            return responseParser.getList(response, resource, params);
         },
-    );
+        async getOne(resource, params): Promise<GetOneResult> {
+            const { path, init } = requestBuilder.getOne(resource, params);
+            const response = await API.get(apiName, path, init);
+            return responseParser.getOne(response, resource, params);
+        },
+        async getMany(resource, params): Promise<GetManyResult> {
+            const { path, init } = requestBuilder.getMany(resource, params);
+            const response = await API.get(apiName, path, init);
+            return responseParser.getMany(response, resource, params);
+        },
+        async getManyReference(
+            resource,
+            params,
+        ): Promise<GetManyReferenceResult> {
+            const { path, init } = requestBuilder.getManyReference(
+                resource,
+                params,
+            );
+            const response = await API.get(apiName, path, init);
+            return responseParser.getManyReference(response, resource, params);
+        },
+        async update(resource, params): Promise<UpdateResult> {
+            const { path, init } = requestBuilder.update(resource, params);
+            const response = await API.put(apiName, path, init);
+            return responseParser.update(response, resource, params);
+        },
+        async updateMany(resource, params): Promise<UpdateManyResult> {
+            //TODO
+            const { path, init } = requestBuilder.updateMany(resource, params);
+            const response = await API.put(apiName, path, init);
+            return responseParser.updateMany(response, resource, params);
+        },
+        async create(resource, params): Promise<CreateResult> {
+            const { path, init } = requestBuilder.create(resource, params);
+            const response = await API.post(apiName, path, init);
+            return responseParser.create(response, resource, params);
+        },
+        async delete(resource, params): Promise<DeleteResult> {
+            const { path, init } = requestBuilder.delete(resource, params);
+            const response = await API.del(apiName, path, init);
+            return responseParser.delete(response, resource, params);
+        },
+        async deleteMany(resource, params): Promise<DeleteManyResult> {
+            //TODO
+            const { path, init } = requestBuilder.deleteMany(resource, params);
+            const response = await API.del(apiName, path, init);
+            return responseParser.deleteMany(response, resource, params);
+        },
+    };
 };
-
-export { buildDataRequest, parseResponse, buildDataProvider };
-
-export * from './types';
