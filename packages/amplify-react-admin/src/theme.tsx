4632bcd53c741758415d94c74c06d6449719ef90
hupe1980 <frankhuebner1980@gmail.com>
2021-12-06 21:28:32 +0100

    Add linter rule to prevent problematic imports

--- packages/amplify-react-admin/src/theme.tsx
+++ packages/amplify-react-admin/src/theme.tsx
@@ -1,4 +1,4 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 declare module '@material-ui/core/styles/createMuiTheme' {
   interface Theme {
