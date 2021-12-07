d4bf760aa6634ab945fbda78531f6360263afae4
hupe1980 <frankhuebner1980@gmail.com>
2021-12-07 08:05:42 +0100

    Fix Linter

--- packages/amplify-material-ui/test/auth/recaptcha.test.tsx
+++ packages/amplify-material-ui/test/auth/recaptcha.test.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 
-jest.mock('react-recaptcha-hook', () => ({
-  ...jest.requireActual('react-recaptcha-hook'),
-  useRecaptcha: () => () => jest.fn().mockResolvedValue("token"),
-}))
-
 import { Recaptcha } from '../../src';
 
-describe('recaptcha', () => {
-  
+jest.mock('react-recaptcha-hook', () => ({
+  ...jest.requireActual('react-recaptcha-hook'),
+  useRecaptcha: () => () => jest.fn().mockResolvedValue('token'),
+}));
 
+describe('recaptcha', () => {
   it('should be rendered correctly', () => {
     const { asFragment } = render(
       <Recaptcha sitekey="KEY" action="ACTION" onToken={(): void => {}} />
